88409f13beb6c00b64b1b8b4120c26a88751866f
Ramil Vildanov <ramvildan.work@gmail.com>
2024-11-09 14:20:55 +0400

    Initial commit

---
+++ src/main/java/com/github/rama4andr/fulfillmentproductapi/FulfillmentProductApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.rama4andr.fulfillmentproductapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulfillmentProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulfillmentProductApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/rama4andr/fulfillmentproductapi/FulfillmentProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rama4andr.fulfillmentproductapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulfillmentProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
